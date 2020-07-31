c8eab48183b9d6e2f16a35551ce592e6caf789d7
jorge alberto burgos hernández <jorge_alberto_burgos@hotmail.com>
2020-07-30 22:56:04 -0600

    avance de formularios

---
+++ src/java/com/proyectobd/servlets/LoginServlet.java
@@ -0,0 +1,79 @@
+
+package com.proyectobd.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) 
+        {
+            String name = request.getParameter("name");
+            String password = request.getParameter("password");
+            String name1 = "cliente";
+            String name2 = "administrador";
+            String password1 = "123456789";
+            String password2 = "000000000";
+            
+            if (name.equals(name1)&&(password.equals(password1))){
+            request.getRequestDispatcher("clienteusuario.html")
+                       .forward(request, response);
+            } 
+            else if (name.equals(name2)&&(password.equals(password2))) {
+                request.getRequestDispatcher("admin.html")
+                       .forward(request, response);
+            }    
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
